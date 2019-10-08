90a8e2021d51c9b5e7f25b246fcb1d4c1a15a73e
Andra Ghitulescu <andra.ghitulescu@gmail.com>
2019-10-08 19:36:07 +0300

    Updates

---
+++ precache-manifest.98230820cccf99d0d5c490b8ec6ce348.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e4cc38ba3e0807480580b46406e57f9",
+    "url": "/DianaParvu/static/media/DianaCameraVideo.9e4cc38b.jpg"
+  },
+  {
+    "revision": "556e5f19bd90bc462d1e",
+    "url": "/DianaParvu/static/css/main.e38f086d.chunk.css"
+  },
+  {
+    "revision": "e19b84f034fdeed6b691",
+    "url": "/DianaParvu/static/js/runtime~main.5c9e4ad7.js"
+  },
+  {
+    "revision": "139c51d09f2953ffa102",
+    "url": "/DianaParvu/static/js/2.18911437.chunk.js"
+  },
+  {
+    "revision": "ef92fede2e5ffca52489c3ed6c5adfa9",
+    "url": "/DianaParvu/static/media/Diana1.ef92fede.jpg"
+  },
+  {
+    "revision": "9426e4e00726436b8108c510ac008b99",
+    "url": "/DianaParvu/static/media/Diana5.9426e4e0.jpg"
+  },
+  {
+    "revision": "556e5f19bd90bc462d1e",
+    "url": "/DianaParvu/static/js/main.e8ab54c5.chunk.js"
+  },
+  {
+    "revision": "c885dede253ca333b1211b31dbe874cf",
+    "url": "/DianaParvu/static/media/Diana4.c885dede.jpg"
+  },
+  {
+    "revision": "c565b91eba8085f750adf842143f29f9",
+    "url": "/DianaParvu/static/media/DianaAndreea.c565b91e.jpg"
+  },
+  {
+    "revision": "18371e8877ac59df797ea180af690bec",
+    "url": "/DianaParvu/static/media/Diana3.18371e88.jpg"
+  },
+  {
+    "revision": "8d2f290aa479833a469bc83f085b6def",
+    "url": "/DianaParvu/static/media/Diana7.8d2f290a.jpg"
+  },
+  {
+    "revision": "6bd5459b0a190b390d6b6da9b4b3e20f",
+    "url": "/DianaParvu/static/media/Cover3.6bd5459b.png"
+  },
+  {
+    "revision": "e37e954b203b694469901208f923e113",
+    "url": "/DianaParvu/index.html"
+  }
+];
